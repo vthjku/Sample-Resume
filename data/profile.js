0e4e3a58928d7b63d03fa6e49fd609c263183e5a
Vipin Yadav <imvpn22@gmail.com>
2022-09-24 19:40:18 +0530

    profile update

--- data/profile.js
+++ data/profile.js
@@ -9,14 +9,14 @@ const profileData = {
       Progressive Web Application standards and application life-cycle managements.`,
     contact: {
       email: "imvpn22@gmail.com",
-      phone: "+91-9919716378",
-      address: "Bangalore, IN",
-    },
+      phone: "+91-9151413108",
+      address: "Bangalore, IN"
+    }
   },
   links: [
     { title: "Codepen", src: "https://codepen.io/imvpn22" },
     { title: "Github", src: "https://github.com/imvpn22" },
-    { title: "LinkedIn", src: "https://www.linkedin.com/in/imvpn22" },
+    { title: "LinkedIn", src: "https://www.linkedin.com/in/imvpn22" }
     // { title: 'Twitter', src: 'https://twitter.com/imvpn22' }
     // { title: 'HackerEarth', src: 'https://www.hackerearth.com/@imvpn22' },
   ],
@@ -25,25 +25,25 @@ const profileData = {
       alma: "FET, GKV, Haridwar",
       duration: "2014 - 2018",
       std: "B.Tech. (Computer Science & Engineering)",
-      score: "71.00%",
+      score: "71.00%"
     },
     {
       alma: "Jwala Devi SVMIC, Prayagraj",
       duration: "2011 - 2013",
       std: "Class XI - XII (PCM)",
-      score: "89.80%",
+      score: "89.80%"
     },
     {
       alma: "Jwala Devi SVMIC, Prayagraj",
       duration: "2011 - 2013",
       std: "Class IX - X (PCM)",
-      score: "73.33%",
-    },
+      score: "73.33%"
+    }
   ],
   skills: [
     {
       category: "Programming Languages",
-      topics: ["TypeScript", "JavaScript"],
+      topics: ["TypeScript", "JavaScript"]
     },
     {
       category: "Frameworks / Libraries",
@@ -52,15 +52,12 @@ const profileData = {
         "Redux",
         "NodeJS",
         "Express",
+        "NextJS",
         "Angular",
         "Bootstrap",
-        "antd",
-      ],
-    },
-    {
-      category: "Databases",
-      topics: ["MongoDB", "PostgreSQL"],
-    },
+        "AntDesign"
+      ]
+    }
   ],
   projects: [
     {
@@ -72,7 +69,7 @@ const profileData = {
       This project was a part of Udacity's <strong>Mobile Web Specialist</strong> nano degree program.
       Developed using <strong>HTML5</strong>, <strong>CSS3</strong> and <strong>JavaScript</strong>
       with Progressive Web-Application standards including full-responsiveness,
-      offline support and improved accessibility.`,
+      offline support and improved accessibility.`
     },
     {
       title: "WhiteBoard",
@@ -82,8 +79,8 @@ const profileData = {
       The app includes a real-time messaging and sketching platform for collaboration with Peers.
       It also has features to create groups(teams) and add/remove members.
       Developed using <strong>Node.js-Express</strong> with server-side-templating (EJS),
-      <strong>CSS</strong>, <strong>JavaScript</strong> and <strong>Socket.io</strong>.`,
-    },
+      <strong>CSS</strong>, <strong>JavaScript</strong> and <strong>Socket.io</strong>.`
+    }
   ],
   experiences: [
     {
@@ -94,8 +91,8 @@ const profileData = {
         `Currently working on the migration of legacy projects from AngularJS codebase to ReactJS codebase with a whole new design by
         developing React based UI component library and utilities, which are being used across different projects in Pricing and Promotion POD.`,
         `Other responsibilities involve adding new features, and patching/fixing bugs on any existing features as well as oncall issues.
-        `,
-      ],
+        `
+      ]
     },
     {
       organization: "Mobile Premier League",
@@ -105,8 +102,8 @@ const profileData = {
         `Worked on the development of internal tools to provide ease in the work of the operation team with tech stack of
         <strong>ReactJS</strong> with <strong>SCSS</strong> and <strong>Redux</strong> for frontend and <strong>NodeJS-Express</strong>
         with protobuf for the backend.`,
-        `Created a customized Content Management System for MPL's website, to update content and images on the fly for different pages.`,
-      ],
+        `Created a customized Content Management System for MPL's website, to update content and images on the fly for different pages.`
+      ]
     },
     {
       organization: "Wipro",
@@ -119,8 +116,8 @@ const profileData = {
         with industry-standard UX patterns using <strong>Angular</strong> with <strong>SCSS</strong>,
         <strong>NgRx</strong> and <strong>Angular-Material</strong> and adding actions to those by
         creating services to integrate backend REST-APIs.`,
-        `Resolved UI/UX issues by refactoring React components and re-structuring CSS/SCSS of an internal project with codebase of ReactJS-Redux.`,
-      ],
+        `Resolved UI/UX issues by refactoring React components and re-structuring CSS/SCSS of an internal project with codebase of ReactJS-Redux.`
+      ]
     },
     {
       organization: "Venuemonk",
@@ -131,8 +128,8 @@ const profileData = {
       details: [
         `Designed and developed a <strong>Venue Onboarding platform</strong> to streamline and automate
         the onboarding process of partner venues using <strong>React.js-Redux</strong> frontend and
-        <strong>Node.js-Express, MongoDB</strong> backend.`,
-      ],
+        <strong>Node.js-Express, MongoDB</strong> backend.`
+      ]
     },
     {
       organization: "Hasura",
@@ -145,23 +142,23 @@ const profileData = {
         <strong>Hasura's BaaS platform</strong> with features of group-chat and a
         sketch board for real-time collaboration with peers. The work was more focused
         towards <strong>using auth and data APIs</strong> offered by the platform, deploying the app,
-        testing and reporting the bugs in it.`,
-      ],
-    },
+        testing and reporting the bugs in it.`
+      ]
+    }
   ],
   certifications: [
     {
       desc: `<strong>Mobile Web Specialist Nanodegree</strong> by <strong>Udacity</strong>.
       (<a target='_blank' rel='noreferrer' href='https://confirm.udacity.com/RLMHXAWZ'>https://goo.gl/RGRzVn</a>)`,
-      date: "May - Nov 2018",
+      date: "May - Nov 2018"
     },
     {
       desc: `<strong>Introduction to Modern Application Development</strong>, a MOOC by <em> NPTEL (IIT Madras)
       and Hasura</em>. Ranked at <strong> top 10 percentile </strong> among more than 2500 candidates.
        (<a target='_blank' rel='noreferrer' href='https://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'>
        https://goo.gl/X3HEdR</a>)`,
-      date: "Jan - Mar 2017",
-    },
+      date: "Jan - Mar 2017"
+    }
   ],
-  events: [],
+  events: []
 };
